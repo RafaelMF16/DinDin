5cebdb7f8ca0bf23c9d55607b0ddb3f355e55f50
RafaelMF16 <rafaelmoreiraferreira63@gmail.com>
2025-01-18 13:19:08 -0300

    Implementei as configurações para usar injeção de dependencia nos testes unitarios

--- DinDin.Tests/BaseTest.cs
+++ DinDin.Tests/BaseTest.cs
@@ -1,7 +1,20 @@
-﻿namespace DinDin.Tests
-{
-    public class BaseTest
-    {
+﻿using Microsoft.Extensions.DependencyInjection;
 
+namespace DinDin.Tests
+{
+    public class BaseTest : IDisposable
+    {
+        protected ServiceProvider ServiceProvider;
+
+        public BaseTest()
+        {
+            var services = new ServiceCollection();
+            InjectionModule.AddServicesInScope(services);
+            ServiceProvider = services.BuildServiceProvider();
+        }
+        public void Dispose()
+        {
+            ServiceProvider.Dispose();
+        }
     }
 }
---
+++ DinDin.Tests/InjectionModule.cs
@@ -0,0 +1,13 @@
+﻿using DinDin.Services.Acconts;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DinDin.Tests
+{
+    public static class InjectionModule
+    {
+        public static void AddServicesInScope(IServiceCollection services)
+        {
+            
+        }
+    }
+}
