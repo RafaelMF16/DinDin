e1e68f48ddcf0399a06e7801bd9e3043dcf4d1bc
RafaelMF16 <rafaelmoreiraferreira63@gmail.com>
2025-01-28 20:03:53 -0300

    Refatorei as entidades

--- DinDin.Domain/Acconts/Accont.cs
+++ DinDin.Domain/Acconts/Accont.cs
@@ -1,10 +1,11 @@
-﻿namespace DinDin.Domain.Acconts
+﻿using DinDin.Domain.MonthlySummaries;
+
+namespace DinDin.Domain.Acconts
 {
     public class Accont
     {
         public int Id { get; set; }
-        public DateTime CreationDate { get; set; }
-        public decimal Balance { get; set; }
         public int UserId { get; set; }
+        public List<MonthlySummary> MonthlySummaries { get; set; } = [];
     }
 }
