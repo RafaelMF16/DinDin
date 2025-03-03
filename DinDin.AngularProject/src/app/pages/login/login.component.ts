56ad64777b508539486110c9700613b730cbfd6e
RafaelMF16 <rafaelmoreiraferreira63@gmail.com>
2025-03-03 13:00:42 -0300

    The reactive form was implemented for login and registration

--- DinDin.AngularProject/src/app/pages/login/login.component.ts
+++ DinDin.AngularProject/src/app/pages/login/login.component.ts
@@ -1,6 +1,5 @@
-import { state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { slideContentTrigger } from '../../animations';
 
 @Component({
@@ -10,10 +9,58 @@ import { slideContentTrigger } from '../../animations';
   styleUrl: './login.component.css',
   animations: [slideContentTrigger]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  
+  loginForm!: FormGroup;
+  registerForm!: FormGroup;
   isLogin: boolean = true;
 
-  onClickInSignUp(): void{
+  constructor(private formBuilder: FormBuilder) { }
+  
+  ngOnInit(): void {
+    this.initializeForms();
+  }
+
+  initializeForms(): void {
+    this.initializeLoginForms();
+    this.initializeRegisterForms();
+  }
+
+  initializeLoginForms(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.email,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
+      password: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(50)
+      ])]
+    });
+  }
+
+  initializeRegisterForms(): void {
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
+      email: ['', Validators.compose([
+        Validators.required, 
+        Validators.email,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
+      password: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(50)
+      ])]
+    });
+  }
+
+  onClickInSignUp(): void {
     this.isLogin = false;
   }
 
