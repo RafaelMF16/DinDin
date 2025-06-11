e6d96a32964fefb50a840f7a429ad0fffb6924a5
RafaelMF16 <rafaelmoreiraferreira63@gmail.com>
2025-06-10 21:55:57 -0300

    Transaction service has been started

---
+++ DinDin.Services/Transactions/TransactionService.cs
@@ -0,0 +1,15 @@
+﻿using DinDin.Domain.Transactions;
+using DinDin.Infra.Transactions;
+
+namespace DinDin.Services.Transactions
+{
+    public class TransactionService(TransactionRepository transactionRepository)
+    {
+        private readonly TransactionRepository _transactionRepository = transactionRepository;
+
+        public async Task<List<Transaction>> GetAllByMonthlySummaryId(int monthlySummaryId)
+        {
+            return await _transactionRepository.GetAllByMonthlySummaryId(monthlySummaryId);
+        }
+    }
+}
